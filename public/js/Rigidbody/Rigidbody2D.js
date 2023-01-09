ab6fdd3620dcf9bda78ebdc69fe8b9a94b8c591d
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-09 23:43:48 +0100

    Apply Forces

--- public/js/Rigidbody/Rigidbody2D.js
+++ public/js/Rigidbody/Rigidbody2D.js
@@ -1,8 +1,11 @@
 class Rigidbody2D{
-    constructor(_useGravity = true){
+    constructor(_useGravity = true, _mass = 1){
         this.velocity = new Vector2(0, 0);
 
         this.useGravity = _useGravity;
+        this.mass = _mass;
+
+        this.force = new Vector2(1, 0);
     };
 
     ExecuteAwake(){
@@ -30,5 +33,9 @@ class Rigidbody2D{
         
         //increase the acceleration
         this.velocity.sub(new Vector2(0, -g));
+    }
+
+    AddForce(force){
+        this.velocity.add(force);
     }
 }
